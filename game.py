6fd0d286d18ca38514d8fdcdb9a0666e0aa120df
Maxwell <cte-berry.gavigan@sllboces.org>
2026-03-05 04:52:48 +0000

    hi :D

--- game.py
+++ game.py
@@ -2,3 +2,8 @@ import movement
 import stats_items
 
 print("~~GAME NAME~~")
+
+name = input("What is your name? ").capitalize()
+if name == "Scarlet":
+    print("How do you know who I am?\n" * 25)
+    print("Shutting down...")
---
+++ stats_items.py
@@ -0,0 +1,4 @@
+player_stats = {"Health": 100, "Stress": 0, "Money": 50}
+
+if command == "stats":
+    print(f"{name}'s stats. Health:{player_stats['Health']} Stress:{player_stats['Stress']} Money:{player_stats['Money']}")
